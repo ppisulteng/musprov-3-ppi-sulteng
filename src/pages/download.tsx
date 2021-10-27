529d3692459235a1897e1f09ec8c28ac8917ed04
morizkay <morizkay@live.com>
2021-10-27 11:22:07 +0800

    Update download.tsx

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -12,6 +12,7 @@ import {
 import { CheckIcon, DownloadIcon } from "@chakra-ui/icons";
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 const data = [
   {
@@ -29,30 +30,35 @@ const data = [
 
 const Download = () => {
   return (
-    <Box p={4}>
-      <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
-        <Heading fontSize={"3xl"}>Dokumen Pendukung Kegiatan</Heading>
-        <Text color={"gray.600"} fontSize={"xl"}>
-          Harap untuk mengunduh dokumen pendukung kegiatan dibawah ini.
-        </Text>
-      </Stack>
-      <Container maxW={"6xl"} mt={10}>
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
-          {data.map((singleData: any) => (
-            <a key={singleData.id} href={singleData.link} download>
-              <HStack align={"top"}>
-                <Box color={"green.400"} px={2}>
-                  <Icon as={DownloadIcon} />
-                </Box>
-                <VStack align={"start"}>
-                  <Text fontWeight={600}>{singleData.title}</Text>
-                </VStack>
-              </HStack>
-            </a>
-          ))}
-        </SimpleGrid>
-      </Container>
-    </Box>
+    <>
+      <Head>
+        <title>Dokumen Musprov PPI Sulteng</title>
+      </Head>
+      <Box p={4}>
+        <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
+          <Heading fontSize={"3xl"}>Dokumen Pendukung Kegiatan</Heading>
+          <Text color={"gray.600"} fontSize={"xl"}>
+            Harap untuk mengunduh dokumen pendukung kegiatan dibawah ini.
+          </Text>
+        </Stack>
+        <Container maxW={"6xl"} mt={10}>
+          <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+            {data.map((singleData: any) => (
+              <a key={singleData.id} href={singleData.link} download>
+                <HStack align={"top"}>
+                  <Box color={"green.400"} px={2}>
+                    <Icon as={DownloadIcon} />
+                  </Box>
+                  <VStack align={"start"}>
+                    <Text fontWeight={600}>{singleData.title}</Text>
+                  </VStack>
+                </HStack>
+              </a>
+            ))}
+          </SimpleGrid>
+        </Container>
+      </Box>
+    </>
   );
 };
 
